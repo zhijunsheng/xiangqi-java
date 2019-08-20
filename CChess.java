a330e51084cb2739e980ee4047d4f7e6cbb6fe88
Peter Shi <petershi@Peters-MacBook-Pro-73.local>
2019-08-20 10:26:09 -0400

    made one row of dots. Yay.

---
+++ CChess.java
@@ -0,0 +1,16 @@
+class CChess {
+  public static void main(String[] args) {
+    CChessBoard brd = new CChessBoard();
+    System.out.println(brd);
+  }
+}
+
+class CChessBoard {
+  public String toString() {
+    String brdStr = "";
+    for (int col = 0; col < 9; col++) {
+      brdStr += ". ";
+    }
+    return brdStr;
+  } 
+}
